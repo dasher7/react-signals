857b9d1f0af39e03eac0a63907a66f20dca5ab11
Andrea Bredice <bredice.andrea@gmail.com>
2024-01-27 16:40:43 +0100

    [CASE 1]: basic codebase to just render todolist in its comps
    backtracked and removed the unecessary code from main branch to show the case of just rendering a memorized list in its components from fetched data that comes from a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { PostsList } from "./components/PostsList";
 import Sidebar from "./components/Sidebar";
-import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  /**
-   * CASE 2 - we want to add the count of the todos in our navbar
-   * Now we need to share the todos across the components
-   * NOTE: in this all components render three times, just the firm time we land on the page
-   */
-  const { posts, updatePosts } = usePosts();
-
   return (
     <div className="App">
-      <Navbar posts={posts} />
-      <Sidebar posts={posts} />
-      <PostsList posts={posts} updatePosts={updatePosts} />
+      <Navbar />
+      <Sidebar />
+      <PostsList />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,5 @@
-import { computed } from "@preact/signals-react";
-import { useSignals } from "@preact/signals-react/runtime";
-import { Post } from "../model/Post";
-import { postSignal } from "./PostsList";
-
-/**
- * CASE 2 - we repeat the same interface
- */
-type NavbarProps = {
-  posts: Post[];
-};
-
-const computedSignal = computed(() => postSignal.value.length);
-
-export const Navbar = (props: NavbarProps) => {
+export const Navbar = () => {
   console.log("RENDERING - Navbar");
-
-  useSignals();
-
-  /* const { posts } = props; */
 
   return (
     <nav className="navbar">
@@ -33,12 +15,6 @@ export const Navbar = (props: NavbarProps) => {
         </li>
         <li>
           <a href="link-4">Link 4</a>
-        </li>
-        <li>
-          <span>
-            Published posts: <strong>{computedSignal}</strong>
-            {/* Published posts: <strong>{posts.length}</strong> */}
-          </span>
         </li>
       </ul>
     </nav>
